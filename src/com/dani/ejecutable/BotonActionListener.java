45da9858e1cef943770c56b809829564b29b7aa0
Daniel Martinez <dani@danielcastelao.org>
2018-05-11 08:51:59 +0200

    Inicio proyecto, creado JPanel Dialogo, añadido icono y ActionListener

---
+++ src/com/dani/ejecutable/BotonActionListener.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dani.ejecutable;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category="File",
+        id="com.dani.ejecutable.BotonActionListener"
+)
+@ActionRegistration(
+        iconBase="com/dani/ejecutable/add.png",
+        displayName="#CTL_BotonActionListener"
+)
+@ActionReference(path="Toolbars/File", position=0)
+@Messages("CTL_BotonActionListener=CrearDeb")
+public final class BotonActionListener implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        // TODO implement action body
+    }
+}
