1ad423a68dc0a479b60eedfd20d32a8903227697
Daniel Martinez <dani@danielcastelao.org>
2018-05-11 09:23:23 +0200

    Introducido código para ejecutar un comando en un programa java

--- src/com/dani/ejecutable/BotonActionListener.java
+++ src/com/dani/ejecutable/BotonActionListener.java
@@ -7,6 +7,8 @@ package com.dani.ejecutable;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
@@ -26,6 +28,21 @@ public final class BotonActionListener implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e){
-        // TODO implement action body
+        String directorio, fichero, clase, extension, dirsalida,nombre;
+
+        directorio=JOptionPane.showInputDialog("Inserte directorio del Jar");
+        fichero=JOptionPane.showInputDialog("Inserte fichero Jar");
+        clase=JOptionPane.showInputDialog("Inserte paquete.Main");
+        extension=JOptionPane.showInputDialog("Inserte extensión (exe/deb)");
+        dirsalida=JOptionPane.showInputDialog("Inserte directorio de salida");
+        nombre=JOptionPane.showInputDialog("Inserte nombre de la nueva app");
+
+        try{
+            String cmd="javapackager -deploy -native "+extension+" -outdir "+dirsalida+" -outfile MiApp -srcdir "+directorio+" -srcfiles "+fichero+" -appclass "+clase+" -name MiApp -title \"MiApp\"";
+            Runtime.getRuntime().exec(cmd);
+            JOptionPane.showMessageDialog(null,"Comando ejecutado con éxito.");
+        }catch(IOException ioe){
+            System.out.println("Error al ejecutar comando");
+        }
     }
 }
