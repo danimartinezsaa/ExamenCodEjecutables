6338e46b4c2dccc75cebde6beda2991eee306aff
Daniel Martinez <dani@danielcastelao.org>
2018-05-11 09:52:22 +0200

    Añadida documentación.

--- src/com/dani/ejecutable/BotonActionListener.java
+++ src/com/dani/ejecutable/BotonActionListener.java
@@ -7,7 +7,9 @@ package com.dani.ejecutable;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.swing.JOptionPane;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
@@ -24,12 +26,20 @@ import org.openide.util.NbBundle.Messages;
 )
 @ActionReference(path="Toolbars/File", position=0)
 @Messages("CTL_BotonActionListener=CrearDeb")
-public final class BotonActionListener implements ActionListener{
 
+/**
+ * Al pulsar el botón dispara la siguiente clase.
+ */
+public final class BotonActionListener implements ActionListener{
+/**
+ * Al darle al botón ejecuta el siguiente método, que pide los datos necesarios para ejecutar el comando.
+ * @param e ActionEvent
+ */
     @Override
     public void actionPerformed(ActionEvent e){
+        //String en dónde se almacenan los datos introducidos por el usuario.
         String directorio, fichero, clase, extension, dirsalida,nombre;
-
+        //Ventanas para introducir los datos.
         directorio=JOptionPane.showInputDialog("Inserte directorio del Jar");
         fichero=JOptionPane.showInputDialog("Inserte fichero Jar");
         clase=JOptionPane.showInputDialog("Inserte paquete.Main");
@@ -38,8 +48,16 @@ public final class BotonActionListener implements ActionListener{
         nombre=JOptionPane.showInputDialog("Inserte nombre de la nueva app");
 
         try{
-            String cmd="javapackager -deploy -native "+extension+" -outdir "+dirsalida+" -outfile MiApp -srcdir "+directorio+" -srcfiles "+fichero+" -appclass "+clase+" -name MiApp -title \"MiApp\"";
-            Runtime.getRuntime().exec(cmd);
+            //En un Array (String) almaceno todo el comando.
+            String [] cmd = {"javapackager","-deploy","-native", extension,"-outdir",dirsalida,"-outfile",nombre,"-srcdir",directorio,"-srcfiles",fichero,"-appclass",clase,"-name",nombre,"-title",nombre};
+            /**
+             * Creo un objeto Process y de la clase Runtime accedo a los métodos estáticos siguientes,
+             * al método exec le paso el Array con el comando
+             */
+            Process process = Runtime.getRuntime().exec(cmd);
+            //con el getInputStream() podemos obtener un Stream para poder leer lo que el comando que ejecutamos escribío en la consola.
+            InputStream inputstream = process.getInputStream();
+            BufferedInputStream bufferedinputstream = new BufferedInputStream(inputstream);
             JOptionPane.showMessageDialog(null,"Comando ejecutado con éxito.");
         }catch(IOException ioe){
             System.out.println("Error al ejecutar comando");
